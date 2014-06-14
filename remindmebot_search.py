1c3cff94dbb71c96f21011e0ffca058307109330
Joey Ranieri <joey.ranieri@gmail.com>
2014-06-13 20:04:25 -0400

    save_to_db() completed, fixed parse_comment a bit

--- remindmebot_search.py
+++ remindmebot_search.py
@@ -24,7 +24,7 @@ USER = config.get("Reddit", "username")
 PASS = config.get("Reddit", "password")
 DB_USER = config.get("SQL", "user")
 DB_PASS = config.get("SQL", "passwd")
-
+DB_TABLE = config.get("SQL", "table")
 # =============================================================================
 # CLASSES
 # =============================================================================
@@ -60,15 +60,16 @@ class Search(object):
 
 	def __init__(self, comment):
 		self.comment = comment # Reddit comment Object
-		# Defaults
-		self._timeDayInt = 1
-		self._timeHourInt = 0
 		self._messageInput = '"Hello, I\'m here to remind you to see the parent comment!"'
+		self._totalTime = 0
 
 	def parse_comment(self):
 		"""
 		Parse comment looking for the message and time
 		"""
+		# Default Times
+		timeDayInt = 1
+		timeHourInt = 0
 
 		# check for hours
 		# regex: 4.0 or 4 "hour | hours" ONLY
@@ -77,20 +78,46 @@ class Search(object):
 		if timeHourTemp:
 			# regex: ignores ".0" and non numbers
 			timeHourTemp = re.search("\d*", timeHourTemp.group(0))
-			self._timeDayInt = int(timeHourTemp.group(0))
+			timeHourInt = int(timeHourTemp.group(0))
 
 		# check for days
 		# regex 4.0 or 4 "day | days" ONLY
 		timeDayTemp = re.search("(?:\d+)?\.*(?:\d+ [dD][aA][yY]([sS]|))", self.comment.body)
 		if timeDayTemp:
 			timeDayTemp = re.search("\d*", timeDayTemp.group(0))
-			self._timeDayInt = int(timeDayTemp.group(0))
+			timeDayInt= int(timeDayTemp.group(0))
 		# cases where the user inputs hours but not days
 		elif not timeDayTemp and timeHourTemp > 0:
-			self._timeDayInt = 0
+			timeDayInt = 0
+
+		# convert into hours
+		self._totalTime = (timeDayInt * 24) + timeHourInt
 
 		# check for user message
 		# regex: Only text around quotes, avoids long messages
 		messageInputTemp = re.search('(["].{0,10000}["])', self.comment.body)
 		if messageInputTemp:
 			self._messageInput = messageInputTemp.group(0)
+
+
+	def save_to_db(self):
+		"""
+		Saves the permalink comment, the time, and the message to the DB
+		"""
+
+		# connection
+		addToDB = Connect()
+
+		# Converting time
+		replyDate = datetime.now(timezone('UTC')) + timedelta(hours=self.hours)
+		#9999/12/31 HH/MM/SS
+		replyDate = format(replyDate, '%Y-%m-%d %H:%M:%S')
+
+		addToDB.execute("INSERT INTO %s VALUES ('%s', %s, '%s', '%s')" %(
+						DB_TABLE, 
+						self.comment.permalink, 
+						self._messageInput, 
+						replyDate, 
+						self.comment.author))
+		addToDB.commit()
+		addToDB.close()
