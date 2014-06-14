178bc50d3242a5d1f85fa5eba41e0a06a88c3eb9
Joey Ranieri <joey.ranieri@gmail.com>
2014-06-13 22:07:09 -0400

    rewrote into a class

--- remindmebot_reply.py
+++ remindmebot_reply.py
@@ -1,8 +1,8 @@
-#! /usr/bin/python -O
-"""
-    Sends a message to the Reddit user after a specified amount of time
+#!/usr/bin/env python2.7
 
-"""
+# =============================================================================
+# IMPORTS
+# =============================================================================
 import praw
 import re
 import MySQLdb
@@ -14,113 +14,145 @@ from praw.errors import ExceptionList, APIException, InvalidCaptcha, InvalidUser
 from socket import timeout
 from pytz import timezone
 
-#Reads the config file
+# =============================================================================
+# GLOBALS
+# =============================================================================
+
+# Reads the config file
 config = ConfigParser.ConfigParser()
 config.read("remindmebot.cfg")
 
-
-user_agent = ("RemindMeBot v1.0 by /u/RemindMeBotWrangler")
-reddit = praw.Reddit(user_agent = user_agent)
-
 #Reddit info
-reddit_user = config.get("Reddit", "username")
-reddit_pass = config.get("Reddit", "password")
-reddit.login(reddit_user, reddit_pass)
+user_agent = ("RemindMeBot v2.0 by /u/RemindMeBotWrangler")
+reddit = praw.Reddit(user_agent = user_agent)
+USER = config.get("Reddit", "username")
+PASS = config.get("Reddit", "password")
 
-#Database info, SQLite used
-host = config.get("SQL", "host")
-user = config.get("SQL", "user")
-passwd = config.get("SQL", "passwd")
-db = config.get("SQL", "db")
-table = config.get("SQL", "table")
+# DB Info
+DB_USER = config.get("SQL", "user")
+DB_PASS = config.get("SQL", "passwd")
+DB_TABLE = config.get("SQL", "table")
+# =============================================================================
+# CLASSES
+# =============================================================================
 
-class Connect:
-	"""
-	DB connection class
-	"""
-	connection = None
-	cursor = None
+class Connect(object):
+    """
+    DB connection class
+    """
+    connection = None
+    cursor = None
 
-	def __init__(self):
-		self.connection = MySQLdb.connect(host= host, user = user, passwd= passwd, db= db)
-		self.cursor = self.connection.cursor()
+    def __init__(self):
+        self.connection = MySQLdb.connect(
+            host="localhost", user=DB_USER, passwd=DB_PASS, db="bot"
+        )
+        self.cursor = self.connection.cursor()
 
-	def execute(self, command):
-		self.cursor.execute(command)
+    def execute(self, command):
+        self.cursor.execute(command)
 
-	def fetchall(self):
-		return self.cursor.fetchall()
+    def fetchall(self):
+        return self.cursor.fetchall()
 
-	def commit(self):
-		self.connection.commit()
+    def commit(self):
+        self.connection.commit()
 
-	def close(self):
-		self.connection.close()
-		
-def time_to_reply():
-	"""
-	Checks to see through SQL if new_date is < current time
-	Once done will send a message to the user
-	"""
-	#connection to DB
-	query_db = Connect()
-	
-	#get current time to compare
-	current_time = datetime.now(timezone('UTC'))
-	current_time = format(current_time, '%Y-%m-%d %H:%M:%S')
-	query_db.execute("SELECT * FROM %s WHERE new_date < '%s'" %(table, current_time))
+    def close(self):
+        self.connection.close()
 
-	data = query_db.fetchall()
-	#row[0] is the permalink to reply to
-	#flag states: 0 is different error, 
-	#1 means comment was succesful, 
-	#2 means comment was deleted
-	already_commented = []
-	for row in data:
-		#checks to make sure permalink hasn't been commented already
-		if row[0] not in already_commented:
-			flag = 0
-			#MySQL-permalink,message, reddit user
-			flag = new_reply(row[0],row[1], row[3])
-			#removes row based on flag
-			if flag == 1 or flag == 2:
-				query_db.execute("DELETE FROM %s WHERE permalink = '%s'" %(table, row[0]))
-				query_db.commit()
-			already_commented.append(row[0])
-	
-	query_db.commit()
-	query_db.close()
+class Reply(object):
 
-def new_reply(permalink, message, author):
-	"""
-	Replies a second time to the user after a set amount of time
-	"""
-	try:
-		comment_to_user = "RemindMeBot here!\n\n**{0}**\n\n {1} \n\n_____\n ^(Hello, I'm RemindMeBot, I will PM you a message so you don't forget about the comment or thread later on!) [^(More Info Here)](http://www.reddit.com/r/RemindMeBot/comments/24duzp/remindmebot_info/)\n\n^(NOTE: Only days and hours. Max wait is one year. Default is a day.)"
-		reddit.send_message(author, 'RemindMeBot Reminder!', comment_to_user.format(message, permalink))
-		return 1
-	except APIException, e:
-		print e
-		if str(e) == "(DELETED_COMMENT) `that comment has been deleted` on field `parent`":
-			return 2
-		return 2
-	except IndexError, e:
-		print e
-		return 2
-	except (HTTPError, ConnectionError, Timeout, timeout), e:
-		print e
-		pass
-	except RateLimitExceeded, e:
-		print e
-		time.sleep(10)
-		pass
+    def __init__(self):
+        self._queryDB = Connect()
+        self._replyMessage =(
+            "RemindMeBot here!\n\n**{0}**\n\n {1} \n\n_____\n"
+            " ^(Hello, I'm RemindMeBot, I will PM you a message so "
+            "you don't forget about the comment or thread later on!) "
+            "[^(More Info Here)]"
+            "(http://www.reddit.com/r/RemindMeBot/comments/24duzp/remindmebot_info/)"
+            "\n\n^(NOTE: Only days and hours. Max wait is one year. Default is a day.)"
+            )
 
-		
+    def time_to_reply(self):
+        """
+        Checks to see through SQL if net_date is < current time
+        """
+
+        # get current time to compare
+        currentTime = datetime.now(timezone('UTC'))
+        currentTime = format(currentTime, '%Y-%m-%d %H:%M:%S')
+        self._queryDB.execute("SELECT * FROM %s WHERE new_date < '%s'" %(DB_TABLE, currentTime))
+
+    def search_db(self):
+        """
+        Loop through data looking for which comments are old
+        """
+
+        data = self._queryDB.fetchall()
+        alreadyCommented = []
+        for row in data:
+            # checks to make sure permalink hasn't been commented already
+            if row[0] not in alreadyCommented:
+                flag = 0
+                # MySQl- permalink, message, reddit user
+                flag = self.new_reply(row[0],row[1], row[3])
+                # removes row based on flag
+                if flag == 1 or flag == 2:
+                    self._queryDB.execute("DELETE FROM %s WHERE permalink = '%s'" %(DB_TABLE, row[0]))
+                    self._queryDB.commit()
+                alreadyCommented.append(row[0])
+
+        self._queryDB.commit()
+        self._queryDB.close()
+    def new_reply(self, permalink, message, author):
+        """
+        Replies a second time to the user after a set amount of time
+        """
+        print self._replyMessage.format(
+                message,
+                permalink
+            )
+        try:
+            reddit.send_message(author, 'RemindMeBot Reminder!', 
+                self._replyMessage.format(
+                    message,
+                    permalink
+                ))
+            return 1
+        except APIException as err:
+            print err
+            return 2
+        except IndexError as err:
+            return 2
+        except (HTTPError, ConnectionError, Timeout, timeout) as err:
+            print err
+            pass
+        except RateLimitExceeded as err:
+            print err
+            time.sleep(10)
+            pass
+
+# =============================================================================
+# MAIN
+# =============================================================================
+
 def main():
-	while True:
-		try:
-			time_to_reply()
-			time.sleep(60*2)
-		except Exception, e:
-			print e	
-main()
+    while True:
+        try:
+            reddit.login(USER, PASS)
+            checkReply = Reply()
+            checkReply.time_to_reply()
+            checkReply.search_db()
+            print "sleep"
+            time.sleep(60*2)
+        except Exception as err:
+            print err 
+
+
+# =============================================================================
+# RUNNER
+# =============================================================================
+print "start"
+if __name__ == '__main__':
+    main()
